cfc1c7f9ebc029ab6e2ecfc6f0dc8ee72be443f2
benfont <114171569+benfont@users.noreply.github.com>
2023-02-18 17:52:08 +0100

    20230218v5

--- utils.c
+++ utils.c
@@ -36,7 +36,39 @@ int	ft_strncmp(char *s1, char *s2, char start, size_t n)
 	return ((unsigned char)s1[i] - (unsigned char) s2[count]);
 }
 
-char	*ft_strjoin(char *line, int fd, )
+char	*ft_strjoin(char *s1, char *s2)
 {
-	
+	int	cont_s1;
+	int	cont_s2;
+	char	*s3;
+
+	s3 = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!s3)
+		return (NULL);
+	cont_s1 = 0;
+	cont_s2 = 0;
+	while (s1[cont_s1])
+	{
+		s3[cont_s1] = s1[cont_s1];
+		cont_s1++;
+	}
+	free(s1);
+	while (s2[cont_s2])
+	{
+		s3[cont_s1 + cont_s2] = s2[cont_s2];
+		cont_s2++;
+	}
+	s3[cont_s1 + cont_s2] = '\0';
+	free(s2);
+	return (s3);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	c;
+
+	c = 0;
+	while (str[c] != '\0')
+		c++;
+	return (c);
 }
